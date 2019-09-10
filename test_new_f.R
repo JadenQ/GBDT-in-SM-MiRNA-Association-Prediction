ea0e60d194bc41975bd2a0786dd5119d839c4aaa
Jaden <qdddyx@163.com>
2019-09-10 08:14:40 +0800

    9.9final

--- test_new_f.R
+++ test_new_f.R
@@ -45,16 +45,19 @@ for(negated in 1 : 1) {
   #############################################
   # build MSA matrix
   loocvKnownMSA <- knownMSA[-negated, ]
+  originalMSA <- matrix(data = rep(0, m * s), nrow = m, ncol = s)
   MSA <- matrix(data = rep(0, m * s), nrow = m, ncol = s)
   for(i in 1 : m) {
     negatedAssociations <- subset(loocvKnownMSA, V2 == i, select = V1)
     for(j in 1 : s) {
       if (j %in% negatedAssociations$V1) {
-        MSA[i, j] <-  1
+        originalMSA[i, j] <-  1
       }
     }
   }
-  
+  MSA <- originalMSA
+
+
   ##########################
   #random walk
   ##########################
@@ -97,49 +100,12 @@ for(negated in 1 : 1) {
     Distance=norm((Ws-LastWs),type=c("2")) #2-r norm
   }
   
-  ###################################
-  #create the network with new weight
-  ###################################
-  
-  #similarity network
-  Mirna_net <- make_empty_graph(n=ALL_MiRNA,directed = FALSE)
-  MiRNANet <-set.vertex.attribute(Mirna_net,"name",value = paste("M",1:ALL_MiRNA,sep = "")) 
-  Sm_net <- make_empty_graph(n=ALL_SM,directed=FALSE)
-  SMNet <- set.vertex.attribute(Sm_net,"name",value = paste("S",1:ALL_SM,sep=""))
-  
-  for(i in 1:m){
-    for(j in 1:i){
-      if(similaritiesOfMiRNA[i,j]>0.4){ #should consider this threshold
-        #link MiRNA
-        MiRNANet<-add_edges(MiRNANet,c(i,j),weight=Wm[i,j])
-      }   
-    }
-  }
-  
-  for(i in 1:s){
-    for(j in 1:i){
-      if(similaritiesOfSM[i,j]>0.41){
-        #link SMs
-        SMNet<-add_edges(SMNet,c(i,j),weight=Ws[i,j])
-      }
-    }
-  }
-  #286+39,541+831
-  All_net<-MiRNANet+SMNet
 
-  #interaction network
-  for(i in 1:m){
-    for(j in 1:s){
-      if(MSA[i,j]==1){
-        All_net<-add_edges(All_net,c(i,m+j),weight=1)
-      }
-    }
-  }
-  all<-simplify(All_net)
   
   #########################
   #create k-similar matrix
   #########################
+  thresholdOfInter=0.8
   Km<-matrix(rep(0,(ALL_MiRNA*3)),nrow=ALL_MiRNA,ncol=3)
   Ks<-matrix(rep(0,(ALL_SM*3)),nrow=ALL_SM,ncol=3)
   KmIndex<-matrix(rep(0,(ALL_MiRNA*3)),nrow=ALL_MiRNA,ncol=3) #index in the graph
@@ -147,11 +113,11 @@ for(negated in 1 : 1) {
   addForGraph<-matrix(rep(ALL_MiRNA,3),1,3)
   for(i in 1:m){
     Km[i,]<-t(as.matrix(sort.int(Wm[i,],decreasing = T,index.return = T)$x[2:4])) # not start at 1 because of the simi of their own
-    KmIndex<-t(as.matrix(sort.int(Wm[i,],decreasing = T,index.return = T)$ix[2:4]))
+    KmIndex[i,]<-t(as.matrix(sort.int(Wm[i,],decreasing = T,index.return = T)$ix[2:4]))
   }
   for(i in 1:s){
     Ks[i,]<-t(as.matrix(sort.int(Ws[i,],decreasing = T,index.return = T)$x[2:4]))
-    KsIndex<-addForGraph+t(as.matrix(sort.int(Ws[i,],decreasing = T,index.return = T)$ix[2:4]))
+    KsIndex[i,]<-addForGraph+t(as.matrix(sort.int(Ws[i,],decreasing = T,index.return = T)$ix[2:4]))
   }
   
   ###########
@@ -160,8 +126,10 @@ for(negated in 1 : 1) {
   for(i in 1:m){
     for(j in 1:s){
       subGraph<-make_empty_graph(n=2,directed=FALSE)
-      subGraph<-set_vertex_attr(subGraph, "name",index=1, value = paste("M",i,sep = ""),label=i)
-      subGraph<-set_vertex_attr(subGraph, "name",index=2, value = paste("S",j,sep = ""),label=(m+j))
+      subGraph<-set_vertex_attr(subGraph, "name",index=1, value = paste("M",i,sep = ""))
+      subGraph<-set_vertex_attr(subGraph,"label",index=1,value=i)
+      subGraph<-set_vertex_attr(subGraph, "name",index=2, value = paste("S",j,sep = ""))
+      subGraph<-set_vertex_attr(subGraph,"label",index=2,value=(m+j))
 
       countS=0
       for(k in 1:s){
@@ -235,13 +203,16 @@ for(negated in 1 : 1) {
     }
   }
   #add weight=1 within new added nodes
-  lineS1=3+countS
+  lineS1=2+countS
   lineS2=lineS1+countS2
-  lineM1=lineS2+countM+1
+  lineM1=lineS2+countM
   lineM2=lineM1+countM2
-  for(k in lineS1:lineS2){
-    for (l in lineM1:lineM2) {
-      if(MSA[V(subGraph)$label[l],(V(subGraph)$label[k]-m)]==1){
+  showTheSub<-V(subGraph)$label
+  for(k in (lineS1+1):lineS2){
+    for (l in (lineM1+1):lineM2) {
+      positionI<-V(subGraph)$label[l]
+      positionII<-V(subGraph)$label[k]-m
+      if(MSA[positionI,positionII]==1){
         subGraph<-add_edges(subGraph,c(k,l),weight=1)
       }
     }
@@ -283,16 +254,16 @@ for(negated in 1 : 1) {
   }
   if(nrow(Type2PathI)>1){
     for(k in 2:nrow(Type2PathI)){
-      if((Type1PathI[k,2]<=ss)&&(Type1PathI[k,3]<=ss)){
+      if((Type2PathI[k,2]<=ss)&&(Type2PathI[k,3]<=ss)){
         C3<-rbind(C3,Type2PathI[k,])
       }
-      else if((Type1PathI[k,2]>ss)&&(Type1PathI[k,3]<=ss)){
+      else if((Type2PathI[k,2]>ss)&&(Type2PathI[k,3]<=ss)){
         C4<-rbind(C4,Type2PathI[k,])
       }
-      else if((Type1PathI[k,2]>ss)&&(Type1PathI[k,3]>ss)){
+      else if((Type2PathI[k,2]>ss)&&(Type2PathI[k,3]>ss)){
         C5<-rbind(C5,Type2PathI[k,])
       }
-      else if((Type1PathI[k,2]<=ss)&&(Type1PathI[k,3]>ss)){
+      else if((Type2PathI[k,2]<=ss)&&(Type2PathI[k,3]>ss)){
         C6<-rbind(C6,Type2PathI[k,])
       }    
     }
@@ -310,13 +281,18 @@ for(negated in 1 : 1) {
     for(k in 2:nrow(C1)){
         EP1=rep(C1[k,],each=2)[-1]
         EP1=EP1[-length(EP1)]
-        C1edgeID[(k-1)),]<-get.edge.ids(subGraph,EP1)
+        C1edgeID[(k-1),]<-get.edge.ids(subGraph,EP1)
       }
     #format the weights
-      C1Weight<-E(subGraph$weight[C1edgeID[1,]])
+    if(nrow(C1edgeID)>1){
+        C1Weight<-as.matrix(E(subGraph)$weight[C1edgeID[1,]])
       for(k in 2:nrow(C1edgeID)){
-        C1Weight<-rbind(C1Weight,E(subGraph)$weight[C1edgeID[k,]])
+        C1Weight<-rbind(C1Weight,as.matrix(E(subGraph)$weight[C1edgeID[k,]]))
       }
+    }
+    else{
+        C1Weight<-as.matrix(E(subGraph)$weight[C1edgeID[1,]])
+    }
 
   }
   else {C1Weight<-matrix(rep(0,2),1)}
@@ -326,80 +302,107 @@ for(negated in 1 : 1) {
     for(k in 2:nrow(C2)){
         EP1=rep(C2[k,],each=2)[-1]
         EP1=EP1[-length(EP1)]
-        C2edgeID[(k-1)),]<-get.edge.ids(subGraph,EP1)
+        C2edgeID[(k-1),]<-get.edge.ids(subGraph,EP1)
       }
     #format the weights
-      C2Weight<-E(subGraph$weight[C2edgeID[1,]])
+    if(nrow(C2edgeID)>1){
+        C2Weight<-as.matrix(E(subGraph)$weight[C2edgeID[1,]])
       for(k in 2:nrow(C2edgeID)){
-        C2Weight<-rbind(C2Weight,E(subGraph)$weight[C2edgeID[k,]])
+        C2Weight<-rbind(C2Weight,as.matrix(E(subGraph)$weight[C2edgeID[k,]]))
       }
+    }
+    else{
+        C2Weight<-as.matrix(E(subGraph)$weight[C2edgeID[1,]])
+    }
 
   }
   else {C2Weight<-matrix(rep(0,2),1)}
 
 
   if(nrow(C3)>1){
-    C3edgeID<-matrix(rep(0,2*(nrow(C3)-1)),(nrow(C3)-1),3)
+    C3edgeID<-matrix(rep(0,3*(nrow(C3)-1)),(nrow(C3)-1),3)
     for(k in 2:nrow(C3)){
         EP1=rep(C3[k,],each=2)[-1]
         EP1=EP1[-length(EP1)]
-        C3edgeID[(k-1)),]<-get.edge.ids(subGraph,EP1)
+        C3edgeID[(k-1),]<-get.edge.ids(subGraph,EP1)
       }
     #format the weights
-      C3Weight<-E(subGraph$weight[C3edgeID[1,]])
+      if(nrow(C3edgeID)>1){
+        C3Weight<-as.matrix(E(subGraph)$weight[C3edgeID[1,]])
       for(k in 2:nrow(C3edgeID)){
-        C3Weight<-rbind(C3Weight,E(subGraph)$weight[C3edgeID[k,]])
+        C3Weight<-rbind(C3Weight,as.matrix(E(subGraph)$weight[C3edgeID[k,]]))
       }
+    }
+    else{
+        C3Weight<-as.matrix(E(subGraph)$weight[C3edgeID[1,]])
+    }
 
   }
   else {C3Weight<-matrix(rep(0,3),1)}
 
   if(nrow(C4)>1){
-    C4edgeID<-matrix(rep(0,2*(nrow(C4)-1)),(nrow(C4)-1),3)
+    C4edgeID<-matrix(rep(0,3*(nrow(C4)-1)),(nrow(C4)-1),3)
     for(k in 2:nrow(C4)){
         EP1=rep(C4[k,],each=2)[-1]
         EP1=EP1[-length(EP1)]
-        C4edgeID[(k-1)),]<-get.edge.ids(subGraph,EP1)
+        C4edgeID[(k-1),]<-get.edge.ids(subGraph,EP1)
       }
     #format the weights
-      C4Weight<-E(subGraph$weight[C4edgeID[1,]])
+    if(nrow(C4edgeID)>1){
+        C4Weight<-as.matrix(E(subGraph)$weight[C4edgeID[1,]])
       for(k in 2:nrow(C4edgeID)){
-        C4Weight<-rbind(C4Weight,E(subGraph)$weight[C4edgeID[k,]])
+        C4Weight<-rbind(C4Weight,as.matrix(E(subGraph)$weight[C4edgeID[k,]]))
       }
+    }
+    else{
+        C4Weight<-as.matrix(E(subGraph)$weight[C4edgeID[1,]])
+    }
+      
 
   }
   else {C4Weight<-matrix(rep(0,3),1)} 
-  
+  #C5
   if(nrow(C5)>1){
-    C5edgeID<-matrix(rep(0,2*(nrow(C5)-1)),(nrow(C5)-1),3)
+    C5edgeID<-matrix(rep(0,3*(nrow(C5)-1)),(nrow(C5)-1),3)
     for(k in 2:nrow(C5)){
         EP1=rep(C5[k,],each=2)[-1]
         EP1=EP1[-length(EP1)]
-        C5edgeID[(k-1)),]<-get.edge.ids(subGraph,EP1)
+        C5edgeID[(k-1),]<-get.edge.ids(subGraph,EP1)
       }
     #format the weights
-      C5Weight<-E(subGraph$weight[C5edgeID[1,]])
+    if(nrow(C5edgeID)>1){
+        C5Weight<-as.matrix(E(subGraph)$weight[C5edgeID[1,]])
       for(k in 2:nrow(C5edgeID)){
-        C5Weight<-rbind(C5Weight,E(subGraph)$weight[C5edgeID[k,]])
+        C5Weight<-rbind(C5Weight,as.matrix(E(subGraph)$weight[C5edgeID[k,]]))
       }
+    }
+    else{
+        C5Weight<-as.matrix(E(subGraph)$weight[C5edgeID[1,]])
+    }
 
   }
   else {C5Weight<-matrix(rep(0,3),1)} 
-
+#C6
   if(nrow(C6)>1){
-    C6edgeID<-matrix(rep(0,2*(nrow(C6)-1)),(nrow(C6)-1),3)
+    C6edgeID<-matrix(rep(0,3*(nrow(C6)-1)),(nrow(C6)-1),3)
     for(k in 2:nrow(C6)){
         EP1=rep(C6[k,],each=2)[-1]
         EP1=EP1[-length(EP1)]
-        C6edgeID[(k-1)),]<-get.edge.ids(subGraph,EP1)
+        C6edgeID[(k-1),]<-get.edge.ids(subGraph,EP1)
       }
     #format the weights
-      C6Weight<-E(subGraph$weight[C6edgeID[1,]])
+    if(nrow(C6edgeID)>1){
+        C6Weight<-as.matrix(E(subGraph)$weight[C6edgeID[1,]])
       for(k in 2:nrow(C6edgeID)){
-        C6Weight<-rbind(C6Weight,E(subGraph)$weight[C6edgeID[k,]])
+        C6Weight<-rbind(C6Weight,as.matrix(E(subGraph)$weight[C6edgeID[k,]]))
       }
+    }
+    else{
+        C6Weight<-as.matrix(E(subGraph)$weight[C6edgeID[1,]])
+    }
 
   }
+
   else {C6Weight<-matrix(rep(0,3),1)}
 
 
@@ -410,49 +413,80 @@ for(negated in 1 : 1) {
   Feature2<-matrix(rep(0,6),1,6)
   Feature3<-matrix(rep(0,6),1,6)   
 
-  pro<-as.matrix(prod(C1Weight[1,]))
-  for(k in 2:nrow(C1Weight)){
-    pro<-rbind(pro,prod(C1Weight[k,]))
+  if(nrow(C1Weight)>1){
+    pro<-as.matrix(prod(C1Weight[1,]))
+    for(k in 2:nrow(C1Weight)){
+      pro<-rbind(pro,prod(C1Weight[k,]))
+   }
   }
+  else{
+    pro<-as.matrix(prod(C1Weight[1,]))
+  }
+
   Feature1[1]<-colSums(pro)
   Feature2[1]<-max(pro)
   Feature3[1]<-(nrow(C1)-1)
 
-  pro<-as.matrix(prod(C2Weight[1,]))
-  for(k in 2:nrow(C2Weight)){
-    pro<-rbind(pro,prod(C2Weight[k,]))
+  if(nrow(C2Weight)>1){
+    pro<-as.matrix(prod(C2Weight[1,]))
+    for(k in 2:nrow(C2Weight)){
+      pro<-rbind(pro,prod(C2Weight[k,]))
+   }
+  }
+  else{
+    pro<-as.matrix(prod(C2Weight[1,]))
   }
   Feature1[2]<-colSums(pro)
   Feature2[2]<-max(pro)
   Feature3[2]<-(nrow(C2)-1)
 
-  pro<-as.matrix(prod(C3Weight[1,]))
-  for(k in 2:nrow(C3Weight)){
-    pro<-rbind(pro,prod(C3Weight[k,]))
+  if(nrow(C3Weight)>1){
+    pro<-as.matrix(prod(C3Weight[1,]))
+    for(k in 2:nrow(C3Weight)){
+      pro<-rbind(pro,prod(C3Weight[k,]))
+   }
+  }
+  else{
+    pro<-as.matrix(prod(C3Weight[1,]))
   }
   Feature1[3]<-colSums(pro)
   Feature2[3]<-max(pro)
   Feature3[3]<-(nrow(C3)-1)
 
-  pro<-as.matrix(prod(C4Weight[1,]))
-  for(k in 2:nrow(C4Weight)){
-    pro<-rbind(pro,prod(C4Weight[k,]))
+  if(nrow(C4Weight)>1){
+    pro<-as.matrix(prod(C4Weight[1,]))
+    for(k in 2:nrow(C4Weight)){
+      pro<-rbind(pro,prod(C4Weight[k,]))
+   }
+  }
+  else{
+    pro<-as.matrix(prod(C4Weight[1,]))
   }
   Feature1[4]<-colSums(pro)
   Feature2[4]<-max(pro)
   Feature3[4]<-(nrow(C4)-1)
 
-  pro<-as.matrix(prod(C5Weight[1,]))
-  for(k in 2:nrow(C5Weight)){
-    pro<-rbind(pro,prod(C5Weight[k,]))
+  if(nrow(C5Weight)>1){
+    pro<-as.matrix(prod(C5Weight[1,]))
+    for(k in 2:nrow(C5Weight)){
+      pro<-rbind(pro,prod(C5Weight[k,]))
+   }
+  }
+  else{
+    pro<-as.matrix(prod(C5Weight[1,]))
   }
   Feature1[5]<-colSums(pro)
   Feature2[5]<-max(pro)
   Feature3[5]<-(nrow(C5)-1)
 
-  pro<-as.matrix(prod(C6Weight[1,]))
-  for(k in 2:nrow(C6Weight)){
-    pro<-rbind(pro,prod(C6Weight[k,]))
+   if(nrow(C6Weight)>1){
+    pro<-as.matrix(prod(C6Weight[1,]))
+    for(k in 2:nrow(C6Weight)){
+      pro<-rbind(pro,prod(C6Weight[k,]))
+   }
+  }
+  else{
+    pro<-as.matrix(prod(C6Weight[1,]))
   }
   Feature1[6]<-colSums(pro)
   Feature2[6]<-max(pro)
@@ -462,5 +496,5 @@ for(negated in 1 : 1) {
 
     }#SUBGRAPH i
   }#subgraph j
-  
+  }
 }
